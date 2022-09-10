65ec0a244f09216078684dd0064175ace5eb8d05
“philippe <philippe@dissitou.org>
2022-09-10 17:31:57 +0200

    Update _define.php

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Google Maps',           					// Name
     'Add custom maps to your blog', 			// Description
     'Philippe aka amalgame and contributors',   // Author
-    '5.9.2',                   					// Version
+    '5.10',                   					// Version
     [
         'requires' => [['core', '2.23']],   	// Dependencies
         'permissions' => 'usage,contentadmin', 	// Permissions
