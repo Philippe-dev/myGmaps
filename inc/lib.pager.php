18f349ae2e006fc9705127583f8ada28df5fde6c
“philippe <philippe@dissitou.org>
2022-10-27 10:34:55 +0200

    Update lib.pager.php

--- inc/lib.pager.php
+++ inc/lib.pager.php
@@ -238,22 +238,22 @@ class adminMapsMiniList extends adminGenericList
     private function postLine($id, $type)
     {
         $img = '<img alt="%1$s" title="%1$s" src="images/%2$s" />';
-        switch ($this->rs->post_status) {
-            case 1:
-                $img_status = sprintf($img, __('Published'), 'check-on.png');
-
+        switch ($rs->post_status) {
+            case dcBlog::POST_PUBLISHED:
+                $img_status = sprintf($img, __('published'), 'check-on.png');
+        
                 break;
-            case 0:
-                $img_status = sprintf($img, __('Unpublished'), 'check-off.png');
-
+            case dcBlog::POST_UNPUBLISHED:
+                $img_status = sprintf($img, __('unpublished'), 'check-off.png');
+        
                 break;
-            case -1:
-                $img_status = sprintf($img, __('Scheduled'), 'scheduled.png');
-
+            case dcBlog::POST_SCHEDULED:
+                $img_status = sprintf($img, __('scheduled'), 'scheduled.png');
+        
                 break;
-            case -2:
-                $img_status = sprintf($img, __('Pending'), 'check-wrn.png');
-
+            case dcBlog::POST_PENDING:
+                $img_status = sprintf($img, __('pending'), 'check-wrn.png');
+        
                 break;
         }
 
