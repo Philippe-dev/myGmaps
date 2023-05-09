b85b6e38414a2b57255c20e47a3b97012fb5e74e
“philippe <philippe@dissitou.org>
2023-05-09 23:15:55 +0200

    Update Install.php

--- src/Install.php
+++ src/Install.php
@@ -20,7 +20,7 @@ class Install extends dcNsProcess
 {
     public static function init(): bool
     {
-        $check  = dcCore::app()->newVersion(My::id(), dcCore::app()->plugins->moduleInfo(My::id(), 'version'));
+        $check = dcCore::app()->newVersion(My::id(), dcCore::app()->plugins->moduleInfo(My::id(), 'version'));
 
         self::$init = My::checkContext(My::INSTALL);
 
