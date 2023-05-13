c58ed77629e8b6f97cf141935a5f33f2e2365dd4
“philippe <philippe@dissitou.org>
2023-05-13 09:11:14 +0200

    Update Backend.php

--- src/Backend.php
+++ src/Backend.php
@@ -254,8 +254,7 @@ class Backend extends dcNsProcess
             $myGmaps_type   = $settings->myGmaps_type;
         }
 
-        $map_js = '<script src="' . DC_ADMIN_URL . '?pf=' . My::id() . '/js/add.map.min.js"></script>' .
-        '<script>' . "\n" .
+        $map_js = dcPage::jsModuleLoad(My::id() . '/js/add.map.min.js') . "\n" .
         '//<![CDATA[' . "\n" .
         'var neutral_blue_styles = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#193341"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#2c5a71"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#29768a"},{"lightness":-37}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#406d80"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#406d80"}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#3e606f"},{"weight":2},{"gamma":0.84}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"weight":0.6},{"color":"#1a3541"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#2c5a71"}]}];' . "\n" .
         'var neutral_blue = new google.maps.StyledMapType(neutral_blue_styles,{name: "Neutral Blue"});' . "\n";
