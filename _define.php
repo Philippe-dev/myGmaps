b76434bc78fc08e135f9b9bddd555e352ebea185
“philippe <philippe@dissitou.org>
2023-05-13 18:20:32 +0200

    Update _define.php

--- _define.php
+++ _define.php
@@ -17,7 +17,7 @@ $this->registerModule(
     'Google Maps',
     'Add custom maps to your blog',
     'Philippe aka amalgame and contributors',
-    '6.3.1',
+    '6.4',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([dcAuth::PERMISSION_CONTENT_ADMIN]),
