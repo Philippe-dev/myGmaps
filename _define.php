78b98bf8eac4c49055f4ca791a9107fd61176f8b
“philippe <philippe.mail.perso@gmail.com>
2024-05-21 09:07:07 +0200

    new version

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'Google Maps',
     'Add custom maps to your blog',
     'Philippe aka amalgame and contributors',
-    '7.1.1',
+    '7.2',
     [
         'requires'    => [['core', '2.30']],
         'permissions' => 'My',
