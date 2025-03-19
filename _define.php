431184f4a7a75c522431932877ed5141d2320baf
“philippe <philippe.mail.perso@gmail.com>
2025-03-19 11:10:51 +0100

    Update _define.php

--- _define.php
+++ _define.php
@@ -15,7 +15,7 @@ $this->registerModule(
     'Philippe aka amalgame and contributors',
     '9.0',
     [
-        'date'     => '2025-03-19T00:00:13+0100',
+        'date'        => '2025-03-19T00:00:13+0100',
         'requires'    => [['core', '2.33']],
         'permissions' => 'My',
         'type'        => 'plugin',
